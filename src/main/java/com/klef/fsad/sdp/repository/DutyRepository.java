41b26c27e2231e69d0ab7c3487f2aee9a25150ec
UmadiAlanka <umadialankam@gmail.com>
2026-01-06 20:44:35 +0530

    Add DutyRepository

---
+++ src/main/java/com/klef/fsad/sdp/repository/DutyRepository.java
@@ -0,0 +1,7 @@
+package com.klef.fsad.sdp.repository;
+
+import com.klef.fsad.sdp.model.Duty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DutyRepository extends JpaRepository<Duty,Integer> {
+}
